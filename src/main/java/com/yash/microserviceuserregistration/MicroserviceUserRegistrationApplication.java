8ec998c4d435f3f76976f7086c3082189d6acfc8
Yash Vaish <yv12350@gmail.com>
2022-02-21 16:56:52 +0530

    Add Eurek Client

--- src/main/java/com/yash/microserviceuserregistration/MicroserviceUserRegistrationApplication.java
+++ src/main/java/com/yash/microserviceuserregistration/MicroserviceUserRegistrationApplication.java
@@ -2,8 +2,10 @@ package com.yash.microserviceuserregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceUserRegistrationApplication {
 
 	public static void main(String[] args) {
